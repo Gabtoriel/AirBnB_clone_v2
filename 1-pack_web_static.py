d8a751f577e8978756bc1265cad96197c91a1911
Gabtoriel <gabtorielvixgmail.com>
2024-01-19 10:08:28 +0000

    Fab file to create an archive of the web static files directory.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,3 +5,21 @@
 """Script to deploy a static on a server using fabric."""
 
 from fabric.api import *
+from datetime import datetime
+import os
+
+
+def do_pack():
+
+    """Creates and stores the archive of a directory."""
+
+    date_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive = "versions/web_static_{}.tgz".format(date_time)
+    local('mkdir -p versions')
+    local('tar -czvf {} web_static'.format(archive))
+    result = os.path.exists(archive)
+    if result is False:
+        return None
+    else:
+        file_size = os.path.getsize(archive)
+        print("web_static packed: {} -> {}Bytes".format(archive, file_size))
